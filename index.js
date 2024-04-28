09759be710855c9978a6c73c60719c42dbb172cc
estebanaugieri <estebanaugieri44@gmail.com>
2024-04-27 21:04:09 -0300

    cambio en app y package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routers = require("./function/routers");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const arrayRutas = [ //routers
     {
